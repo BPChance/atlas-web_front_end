58d9ac0dc871d58dbd6a37e0852c62ba1640d9ba
BPChance <bradenpchance6@gmail.com>
2024-11-18 19:41:08 -0600

    prime numbers and timing execution

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+    let count = 0;
+
+    // function to check if number is prime
+    function primeNumber(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    // prime numbers from 2-100
+    for (let i = 2; i <= 100; i++) {
+        if (primeNumber(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const start = performance.now();
+const primeCount = countPrimeNumbers();
+const end = performance.now();
+
+console.log(`Execution time of printing countPrimeNumbers was ${(end - start)} milliseconds.`);
