ea6b999a38f7f638e7acb39956dfc1ec38813ccb
BPChance <bradenpchance6@gmail.com>
2024-11-18 18:17:51 -0600

    pushing 2-7, not 5

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,45 @@
+class studentHogwarts {
+    constructor() {
+        let privateScore = 0;
+        let name = null;
+
+        function changeScoreBy(points) {
+            privateScore += points;
+        }
+
+        this.setName = function (newName) {
+            name = newName;
+        };
+
+        this.rewardStudent = function () {
+            changeScoreBy(1);
+        };
+
+        this.penalizeStudent = function () {
+            changeScoreBy(-1);
+        };
+
+        this.getScore = function () {
+            return `${name}: ${privateScore}`;
+        };
+    }
+}
+
+const harry = new studentHogwarts();
+harry.setName("Harry");
+
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+
+console.log(harry.getScore());
+
+const draco = new studentHogwarts();
+draco.setName("Draco");
+
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+
+console.log(draco.getScore());
