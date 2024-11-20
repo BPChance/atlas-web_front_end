c2af1132f9756b55ea8931a6c6b46e2095e680b4
BPChance <bradenpchance6@gmail.com>
2024-11-19 19:08:40 -0600

    11-14, 14 not great

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,32 @@
+function countPrimeNumbers() {
+    let count = 0;
+
+    // function to check if number is prime
+    function primeNumber(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    // prime numbers from 2-100
+    for (let i = 2; i <= 100; i++) {
+        if (primeNumber(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+setTimeout(() => {
+    const start = performance.now();
+
+    for (let i = 0; i < 100; i++) {
+        countPrimeNumbers();
+    }
+
+    const end = performance.now();
+
+    console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds.`);
+}, 0);
